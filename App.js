191f89bc75a81b7ae2c25150d92082e2fec9eafe
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-04-15 23:58:36 +0300

    box added

--- App.js
+++ App.js
@@ -4,32 +4,35 @@ import {View, Text, SafeAreaView, Button, StyleSheet} from 'react-native';
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.upperView}>
-        <Text> Hello World </Text>
-      </View>
-      <View style={styles.middleView}>
-        <Text> Merhaba Dünya </Text>
-      </View>
-      <View style={styles.bottomView}>
-        <Text> Merhaba Dünya </Text>
-      </View>
+      <View style={styles.box_1} />
+      <View style={styles.box_2} />
+      <View style={styles.box_3} />
+      <View style={styles.box_4} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1, flexDirection: 'row'},
-  upperView: {
+  box_1: {
     backgroundColor: 'blue',
-    flex: 3,
+    width: 75,
+    height: 75,
   },
-  bottomView: {
+  box_2: {
     backgroundColor: 'yellow',
-    flex: 1,
+    width: 75,
+    height: 75,
   },
-  middleView: {
+  box_3: {
+    backgroundColor: 'green',
+    width: 75,
+    height: 75,
+  },
+  box_4: {
     backgroundColor: 'red',
-    flex: 2,
+    width: 75,
+    height: 75,
   },
 });
 
