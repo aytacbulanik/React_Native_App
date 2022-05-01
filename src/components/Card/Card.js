2aecc48b543bdd4181f31550412ea8e0626b5a50
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-01 16:53:25 +0300

    custom comp style created

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'React-native';
+import style from './Card.style';
+
+const app = () => {
+  return (
+    <View style={style.cardContainer}>
+      <View>
+      <Text>Joe Byden</Text>
+      <Text> Nasılsınız Dünya</Text>
+      </View>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+      
+    </View>
+  );
+};
+
+export default app;
---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    backgroundColor: '#e0e0e0',
+  },
+  cardView: {
+    backgroundColor: 'white',
+  },
+});
+
+export default style;
