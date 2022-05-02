0549a83ba1d050be945e80b4702c20f0078ef4b0
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-03 00:38:19 +0300

    like button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {View, SafeAreaView, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 function App() {
   return (
     <SafeAreaView style={style.safeAreaContainer}>
-      <View style={style.cardView}>
-        <View>
-          <Text>Joe Byden</Text>
-          <Text> Nasılsınız Dünya</Text>
+      <View>
+        <View style={style.cardView}>
+          <Text style={style.titleText}>Joe Byden</Text>
+          <Text style={style.wordsText}> Nasılsınız Dünya</Text>
         </View>
+        <TouchableOpacity>
+          <Text style={style.buttonContainer}>I LIKED</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -21,11 +30,25 @@ const style = StyleSheet.create({
   },
   cardView: {
     backgroundColor: 'white',
-    margin: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: 'gray',
     borderRadius: 10,
+  },
+  titleText: {
+    margin: 5,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  wordsText: {
+    color: 'gray',
+    fontSize: 15,
+    margin: 5,
+  },
+  buttonContainer: {
+    backgroundColor: 'blue',
+    color: 'white',
+    alignItems: 'center',
   },
 });
 
