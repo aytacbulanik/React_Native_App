46a2c7f2862d34a943a131f1201b4a902fa6ec6c
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-04 20:39:39 +0300

    custom comp completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import {
-  View,
-  SafeAreaView,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Card from './components/Card';
 
 function App() {
   return (
     <SafeAreaView style={style.safeAreaContainer}>
-      <View style={style.cardContainer}>
-        <View style={style.cardBody}>
-          <Text style={style.titleText}>Joe Byden</Text>
-          <Text style={style.wordsText}> Nasılsınız Dünya</Text>
-        </View>
-        <TouchableOpacity style={style.buttonContainer}>
-          <Text>I LIKED</Text>
-        </TouchableOpacity>
-      </View>
-      <Card />
+      <Card title="Joe Biden" words="Dünya benden korksun" />
+      <Card title="Arif Işık" words="Uzaylılar tarafından kaçırıldım" />
+      <Card title="Sıla" words="Yıkılmışım ben" />
     </SafeAreaView>
   );
 }
@@ -29,38 +16,6 @@ const style = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
     backgroundColor: '#e0e0e0',
-  },
-  cardContainer: {
-    margin: 10,
-  },
-  cardBody: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 10,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  titleText: {
-    margin: 5,
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  wordsText: {
-    color: 'gray',
-    fontSize: 15,
-    margin: 5,
-  },
-  buttonContainer: {
-    backgroundColor: '#00c2ff',
-    padding: 10,
-    color: 'white',
-    fontWeight: '600',
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-    fontSize: 20,
-    alignItems: 'center',
   },
 });
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Alert} from 'react-native';
 import style from './Card.style';
 
-const Card = () => {
+const Card = props => {
   return (
     <View style={style.container}>
       <View style={style.body}>
-        <Text style={style.title}>Elif Şanlı</Text>
-        <Text style={style.words}> Nasılsınız Dünya</Text>
+        <Text style={style.title}>{props.title}</Text>
+        <Text style={style.words}> {props.words}</Text>
       </View>
-      <TouchableOpacity style={style.buttonContainer}>
+      <TouchableOpacity
+        style={style.buttonContainer}
+        onPress={() => Alert.alert('Merhaba')}>
         <Text style={style.buttonText}>I LIKED</Text>
       </TouchableOpacity>
     </View>
