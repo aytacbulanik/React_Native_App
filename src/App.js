6dbb6cc146d7365956dd23e12189446a3fdbc3b9
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-08 18:54:03 +0300

    banner image has come

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  ScrollView,
+  Image,
+  Dimensions,
+} from 'react-native';
 import NewsCard from './components/NewsCard';
 import news_data from './news_data.json';
+import news_banner_data from './news_banner_data.json';
 
 function App() {
+  const renderItems = ({item}) => <NewsCard news={item} />;
   return (
     <SafeAreaView style={style.safeAreaContainer}>
-      <Text>Flatlist Projesi</Text>
-      <FlatList
-        data={news_data}
-        renderItem={({item}) => <NewsCard news={item} />}
-      />
+      <ScrollView horizontal>
+        {news_banner_data.map(newsbanner => (
+          <Image
+            style={style.bannerImage}
+            source={{uri: newsbanner.imageUrl}}
+          />
+        ))}
+      </ScrollView>
+      <FlatList data={news_data} renderItem={renderItems} />
     </SafeAreaView>
   );
 }
@@ -19,6 +33,10 @@ const style = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
     backgroundColor: '#e0e0e0',
+  },
+  bannerImage: {
+    width: Dimensions.get('window').width / 3,
+    height: Dimensions.get('window').height / 4,
   },
 });
 
