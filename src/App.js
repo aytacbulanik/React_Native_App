672b6216b46cac5c5b8c5f06702deacfb0285aeb
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-28 11:24:56 +0300

    useEffect added and working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
 
 function App() {
   const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log('number changed');
+  }, [number]);
   return (
     <SafeAreaView>
       <Text> Deneme sayısı</Text>
       <Text> Sayı : {number}</Text>
-      <Button title='Arttır' onPress={() => setNumber(number + 2)}/>
+      <Button title="Arttır" onPress={() => setNumber(number + 2)} />
     </SafeAreaView>
   );
 }
